64da8d53ba7299c644370de7cb9113bd3a9fcd3f
+Sam Thongo <+samuelthongo73@gmail.com>
2023-10-10 13:21:19 +0300

    converting lists

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+    if len(elements) == 1:
+        return elements[0]
+    elif len(elements) == 2:
+        return f"{elements[0]} and {elements[1]}"
+    else:
+        comma_list = ", ".join(elements[:-1])
+    return comma_list + f", and {elements[-1]}"
+print(oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]))
